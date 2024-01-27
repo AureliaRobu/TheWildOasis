d37d3dcd634186e1b3c2479446c2b815a420bf4e
Aurelia Robu <robuaurelia@gmail.com>
2024-01-27 07:38:49 +0200

    Fetching app settings

--- src/features/cabins/CabinRow.tsx
+++ src/features/cabins/CabinRow.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 import { useState } from 'react';
+import { HiPencil, HiSquare2Stack, HiTrash } from 'react-icons/hi2';
 import { formatCurrency } from '../../utils/helpers';
 import CreateCabinForm from './CreateCabinForm';
 import useDeleteCabin from './useDeleteCabin';
+import useCreateCabin from './useCreateCabin';
 
 const TableRow = styled.div`
   display: grid;
@@ -51,10 +53,23 @@ function CabinRow({ cabin }) {
     maxCapacity,
     discount,
     name,
+    description,
   } = cabin;
 
   const [showForm, setShowForm] = useState(false);
   const { isDeleting, deleteCabin } = useDeleteCabin();
+  const { isCreating, createCabin } = useCreateCabin();
+
+  function handleDuplicate() {
+    createCabin({
+      name: `Copy of cabin ${name}`,
+      image,
+      regularPrice,
+      maxCapacity,
+      discount,
+      description,
+    });
+  }
 
   return (
     <>
@@ -69,9 +84,14 @@ function CabinRow({ cabin }) {
           <span>&mdash;</span>
         )}
         <div>
-          <button onClick={() => setShowForm((show) => !show)}>Edit</button>
+          <button disabled={isCreating} onClick={handleDuplicate}>
+            <HiSquare2Stack />
+          </button>
+          <button onClick={() => setShowForm((show) => !show)}>
+            <HiPencil />
+          </button>
           <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>
-            Delete
+            <HiTrash />
           </button>
         </div>
       </TableRow>
