d5e324b2872ed41b7c044d473820e024dc62bf4f
Aurelia Robu <robuaurelia@gmail.com>
2024-02-08 08:38:39 +0200

    Reusable table

--- src/features/cabins/CabinRow.tsx
+++ src/features/cabins/CabinRow.tsx
@@ -5,19 +5,8 @@ import CreateCabinForm from './CreateCabinForm';
 import useDeleteCabin from './useDeleteCabin';
 import useCreateCabin from './useCreateCabin';
 import Modal from '../../ui/Modal';
-import ConfirmDelete from '../../ui/ConfirmDelete.tsx';
-
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+import ConfirmDelete from '../../ui/ConfirmDelete';
+import Table from '../../ui/Table';
 
 const Img = styled.img`
   display: block;
@@ -72,7 +61,7 @@ function CabinRow({ cabin }) {
   }
 
   return (
-    <TableRow role="row">
+    <Table.Row>
       <Img src={image} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity}</div>
@@ -95,12 +84,12 @@ function CabinRow({ cabin }) {
           <Modal.Window name="cabin-edit">
             <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
-          <Modal.Open opens='cabin-delete'>
+          <Modal.Open opens="cabin-delete">
             <button>
               <HiTrash />
             </button>
           </Modal.Open>
-          <Modal.Window name='cabin-delete'>
+          <Modal.Window name="cabin-delete">
             <ConfirmDelete
               resourceName="cabins"
               disabled={isDeleting}
@@ -109,7 +98,7 @@ function CabinRow({ cabin }) {
           </Modal.Window>
         </Modal>
       </div>
-    </TableRow>
+    </Table.Row>
   );
 }
 
