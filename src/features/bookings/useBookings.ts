a92f836d49fe932d30fa591938a360b3e76169bb
Aurelia Robu <robuaurelia@gmail.com>
2024-02-21 08:20:04 +0200

    Bookings sorting API side

--- src/features/bookings/useBookings.ts
+++ src/features/bookings/useBookings.ts
@@ -4,20 +4,24 @@ import { getBookings } from '../../services/apiBookings';
 
 export default function useBookings() {
   const [searchParams] = useSearchParams();
+  // Filter
   const filterValue = searchParams.get('status');
-  console.log(filterValue);
   const filter =
     !filterValue || filterValue === 'all'
       ? null
       : { field: 'status', value: filterValue };
-  console.log(filter);
+
+  // Sort
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = { field, direction };
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings', filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { isLoading, bookings, error };
 }
